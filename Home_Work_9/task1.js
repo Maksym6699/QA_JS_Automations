00e9c4d403d225c803f26ed8d6008e95a6d59787
Maksym6699 <kozyrmaxim20@gmail.com>
2026-04-03 11:58:55 +0300

    Added HomeWork9

---
+++ Home_Work_9/task1.js
@@ -0,0 +1,8 @@
+let student = {};
+
+student.firstName = 'Max';
+student.lastName = 'Smith';
+student.age = 18;
+student.courses = ['Математика', 'Історія', 'Програмування'];
+
+console.log(student);
---
+++ Home_Work_9/task2.js
@@ -0,0 +1,10 @@
+const book = {
+  title: 'Kolobok',
+  author: 'NoName',
+  year: '2023',
+};
+
+const { title, author } = book;
+
+console.log('Назва книги:', title);
+console.log('Автор книги:', author);
